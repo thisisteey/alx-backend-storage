3ac6b0094878cdb9f208293ec0c482c23b624545
thisisteey <dadataiwo82@yahoo.com>
2024-06-17 05:13:24 -0700

    Feature: Python script that provides some stats about Nginx logs stored in MongoDB

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -3,18 +3,18 @@
 from pymongo import MongoClient
 
 
-def nginx_log_statistics(mongo_collection):
+def nginx_log_statistics(nginx_collection):
     """Gets and prints some stats about Nginx logs in MongoDB"""
-    total_logs = mongo_collection.count_documents({})
-    print("{} logs".format(total_logs))
+    num_logs = nginx_collection.count_documents({})
+    print("{} logs".format(num_logs))
     print("Methods:")
-    http_methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
-    for method in http_methods:
-        method_count = mongo_collection.count_documents({"method": method})
-        print("\tmethod {}: {}".format(method, method_count))
-    status_route_count = mongo_collection.count_documents({"method": "GET",
-                                                           "path": "/status"})
-    print("{} status_check".format(status_route_count))
+    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    for method in methods:
+        docs = nginx_collection.count_documents({"method": method})
+        print("\tmethod {}: {}".format(method, docs))
+    route = nginx_collection.count_documents({"method": "GET",
+                                              "path": "/status"})
+    print("{} status check".format(route))
 
 
 if __name__ == "__main__":
