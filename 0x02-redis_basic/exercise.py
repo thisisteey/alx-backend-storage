6d278ebb4b9b63f2e09e455bb209e7add4840997
thisisteey <dadataiwo82@yahoo.com>
2024-06-19 14:02:09 +0000

    Feature: Add get method with optional callable and implement get_str and get_int methods

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -2,7 +2,8 @@
 """Module for using the NoSQL data storage Redis"""
 import redis
 import uuid
-from typing import Union
+from typing import Union, Callable
+
 
 class Cache:
     """A class to manage data storage in a Redis database"""
@@ -16,3 +17,17 @@ class Cache:
         randomRedisKey = str(uuid.uuid4())
         self._redis.set(randomRedisKey, data)
         return randomRedisKey
+
+    def get(self, key: str, fn: Callable = None)\
+            -> Union[str, bytes, int, float]:
+        """Gets and retrieves a value from the database using a key"""
+        data = self._redis.get(key)
+        return fn(data) if fn is not None else data
+
+    def get_str(self, key: str) -> str:
+        """Gets and retrieves a value from the database as a string"""
+        return self.get(key, lambda q: q.decode('utf-8'))
+
+    def get_int(self, key: str) -> int:
+        """Gets and retrieves a value from the database as an integer"""
+        return self.get(key, lambda q: int(q))
